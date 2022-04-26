c362d3df5e3600df00c091127a4b0549849f7a5e
bokdol11859 <2019147551@yonsei.ac.kr>
2022-04-27 01:59:01 +0900

    init

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StatusBar, View, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView
+        style={{ flex: "1", backgroundColor: "#FBFBFB" }}
+        edges={["top"]}
+      >
+        <WebView
+          source={{ uri: "https://myplanit.site" }}
+          // userAgent="Mozilla/5.0 (IPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15(KHTML, like Gecko) CriOS/80.0.3987.95 Mobile/15E148 Safari/604.1"
+        />
+        <StatusBar barStyle="dark-content" backgroundColor="#FBFBFB" />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "ios" ? StatusBar.currentHeight : 0,
   },
 });
