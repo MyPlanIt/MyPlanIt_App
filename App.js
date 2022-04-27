00ac0134943b45806b4509652d1cc5005f18fc43
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-04-27 19:10:39 +0900

    back button blocked, design for ios and android separated

--- App.js
+++ App.js
@@ -1,29 +1,49 @@
-import React, { Component } from "react";
-import { StatusBar, View, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import {
+  StatusBar,
+  View,
+  StyleSheet,
+  BackHandler,
+  Alert,
+  Platform,
+} from "react-native";
 import { WebView } from "react-native-webview";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <>
-      <SafeAreaView
-        style={{ flex: "1", backgroundColor: "#FBFBFB" }}
-        edges={["top"]}
-      >
-        <WebView
-          source={{ uri: "https://myplanit.site" }}
-          // userAgent="Mozilla/5.0 (IPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15(KHTML, like Gecko) CriOS/80.0.3987.95 Mobile/15E148 Safari/604.1"
-        />
-        <StatusBar barStyle="dark-content" backgroundColor="#FBFBFB" />
-      </SafeAreaView>
-    </>
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("", "앱을 종료하시겠습니까?", [
+        {
+          text: "취소",
+          onPress: () => null,
+        },
+        { text: "확인", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
+  return Platform.OS === "ios" ? (
+    <SafeAreaView style={{ flex: 1 }} edges={["top"]} backgroundColor="#FBFBFB">
+      <WebView source={{ uri: "https://myplanit.site" }} />
+      <StatusBar barStyle="dark-content" backgroundColor="#FBFBFB" />
+    </SafeAreaView>
+  ) : (
+    <View style={{ flex: 1, marginTop: 30 }}>
+      <WebView source={{ uri: "https://myplanit.site" }} />
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "ios" ? StatusBar.currentHeight : 0,
-  },
-});
