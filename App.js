87f51eff3726c4f1d6495ecea4d4cc259282d626
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-05-02 16:19:01 +0900

    Google Login fixed

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StatusBar,
   View,
@@ -9,9 +9,14 @@ import {
 } from "react-native";
 import { WebView } from "react-native-webview";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import * as Device from "expo-device";
 
 export default function App() {
+  const [userDevice, setUserDevice] = useState("");
+
   useEffect(() => {
+    setUserDevice(Device.modelName);
+
     const backAction = () => {
       Alert.alert("", "앱을 종료하시겠습니까?", [
         {
@@ -33,12 +38,18 @@ export default function App() {
 
   return Platform.OS === "ios" ? (
     <SafeAreaView style={{ flex: 1 }} edges={["top"]} backgroundColor="#FBFBFB">
-      <WebView source={{ uri: "https://myplanit.site" }} />
+      <WebView
+        userAgent={`Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Mobile/15E148 Safari/604.1`}
+        source={{ uri: "https://myplanit.site" }}
+      />
       <StatusBar barStyle="dark-content" backgroundColor="#FBFBFB" />
     </SafeAreaView>
   ) : (
     <View style={{ flex: 1, marginTop: 30 }}>
-      <WebView source={{ uri: "https://myplanit.site" }} />
+      <WebView
+        userAgent={`Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Mobile Safari/537.36`}
+        source={{ uri: "https://myplanit.site" }}
+      />
       <StatusBar
         translucent
         barStyle="dark-content"
