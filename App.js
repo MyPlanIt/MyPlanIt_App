3247b80b423f1560dcb3ce5c870a36d8f55364b7
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-05-08 17:57:02 +0900

    history fix

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import {
   StatusBar,
   View,
@@ -11,21 +11,29 @@ import { WebView } from "react-native-webview";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  const webview = useRef(null);
-
-  useEffect(() => {
-    const backAction = () => {
+  const webview = useRef();
+  const [isCanGoBack, setIsCanGoBack] = useState(false);
+  const onPressHardwareBackButton = () => {
+    if (isCanGoBack) {
       webview.current.goBack();
       return true;
-    };
+    } else {
+      return false;
+    }
+  };
 
-    const backHandler = BackHandler.addEventListener(
+  useEffect(() => {
+    BackHandler.addEventListener(
       "hardwareBackPress",
-      backAction
+      onPressHardwareBackButton
     );
-
-    return () => backHandler.remove();
-  }, []);
+    return () => {
+      BackHandler.removeEventListener(
+        "hardwareBackPress",
+        onPressHardwareBackButton
+      );
+    };
+  }, [isCanGoBack]);
 
   return Platform.OS === "ios" ? (
     <SafeAreaView style={{ flex: 1 }} edges={["top"]} backgroundColor="#FBFBFB">
@@ -41,6 +49,36 @@ export default function App() {
         userAgent={`Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Mobile Safari/537.36`}
         source={{ uri: "https://myplanit.site" }}
         ref={webview}
+        injectedJavaScript={`
+        (function() {
+          function wrap(fn) {
+            return function wrapper() {
+              var res = fn.apply(this, arguments);
+              window.ReactNativeWebView.postMessage(window.location.href);
+              return res;
+            }
+          }
+    
+          history.pushState = wrap(history.pushState);
+          history.replaceState = wrap(history.replaceState);
+          window.addEventListener('popstate', function() {
+            window.ReactNativeWebView.postMessage(window.location.href);
+          });
+        })();
+    
+        true;
+      `}
+        onMessage={({ nativeEvent: state }) => {
+          console.log(state.data);
+          if (state.data !== "https://www.myplanit.site/todo") {
+            // Navigation state updated, can check state.canGoBack, etc.
+            setIsCanGoBack(state.canGoBack);
+            console.log(isCanGoBack);
+          } else {
+            setIsCanGoBack(false);
+            console.log(isCanGoBack);
+          }
+        }}
       />
       <StatusBar
         translucent
